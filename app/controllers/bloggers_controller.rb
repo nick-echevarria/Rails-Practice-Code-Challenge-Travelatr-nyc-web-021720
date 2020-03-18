1ee971f91f21c81baff6cd5fc8d581428e5f0c37
theslickestnick <nick.echev@gmail.com>
2020-03-18 17:26:32 -0400

    Completed Posts Controller + Views

--- app/controllers/bloggers_controller.rb
+++ app/controllers/bloggers_controller.rb
@@ -1,29 +1,44 @@
 class BloggersController < ApplicationController
     def index
-
+        @bloggers = Blogger.all
     end
 
     def show
-
+        @blogger = Blogger.find(params[:id])
     end
 
     def new
-
+        @blogger = Blogger.new
     end
 
     def create
-
+        @blogger = Blogger.create(blogger_params)
+        if @blogger.valid?
+            redirect_to blogger_path(@blogger)
+        else
+            flash[:errors] = @blogger.errors.full_messages
+            redirect_to new_blogger_path
+        end
     end
 
-
     def update 
-
+        @blogger = Blogger.find(params[:id])
+        if @blogger.update(blogger_params)
+            redirect_to blogger_path(@blogger)
+        else 
+            flash[:errors] = @blogger.errors.full_messages
+            redirect_to edit_blogger_path
+        end
     end
 
     def edit
-
+        @blogger = Blogger.find(params[:id])
     end
     
- 
+    private 
+
+    def blogger_params
+        params.require(:blogger).permit(:name, :age, :bio)
+    end
    
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,26 +5,57 @@ class PostsController < ApplicationController
     end
 
     def show
+        @post = Post.find(params[:id])
+        @destination = Destination.find(params[:id])
+        @blogger = Blogger.find(params[:id])
 
+        
     end
 
     def new
-
+        @post = Post.new
+        @destinations = Destination.all
+        @bloggers = Blogger.all
     end
 
     def create
+        @post = Post.create(post_params)        
+        # if @post.valid?
+            redirect_to post_path(@post)
+        # else 
+        #     flash[:errors] = @post.errors.full_messages
+        #     redirect_to new_post_path
+        # end
 
+        # @post = Post.new(post_params)
+        # if @post.save
+        #     redirect_to post_path(@post)
+        # else
+        #     flash[:errors] = @post.errors.full_messages
+        #     redirect_to edit_post_path
+        # end
     end
 
-
     def update 
-
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to post_path(@post)
+        else
+            flash[:errors] = @post.errors.full_messages
+            redirect_to edit_post_path(@post)
+        end
     end
 
     def edit
-
+        @post = Post.find(params[:id])
     end
     
+    private
+
+    def post_params 
+        params.require(:post).permit(:title, :content, :blogger_id, :destination_id)
+    end
+
     # def destroy
 
     # end
